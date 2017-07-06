ae8211bc214d4121b6364399f64ca29f54db81b8
Kaili Nishihira and Robert Murray <robert@>
2017-07-06 11:45:33 -0700

    Add functionality to radio buttons, contact information, submit button, confirmation section.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var diamond = $("input:radio[name=diamond]:checked").val();
+    var nameInput = $("input#name").val();
+    var addressInput = $("input#address").val();
+    var emailInput = $("input#email").val();
+    var phoneInput = $("input#phone").val();
 
+    $(".diamond").text(diamond);
+    $(".name").text(nameInput);
+    $(".address").text(addressInput);
+    $(".email").text(emailInput);
+    $(".phone").text(phoneInput);
+
+    $("#confirmation").show();
+    event.preventDefault();
+  });
 });
